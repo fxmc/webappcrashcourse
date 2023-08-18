c0707dc8843d7e28863f55c1bcab5b5725c31b21
Corvin Codirla <corvin@codirla.com>
2023-08-18 00:26:19 -0500

    Finished the reading from backend database, and hidden apis and urls.

--- script.js
+++ script.js
@@ -1,55 +1,72 @@
 "use strict";
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
+import { API_KEY, SUPA_URL } from "./env.js";
+import { CATEGORIES } from "./const.js";
 
-// Selectin DOM elements
-const btn = document.querySelector(".btn-open");
-const form = document.querySelector(".fact-form");
+// ********************** Filling out the list of facts
+
+// Selecting the fact list and blanking it out
 const facts_list = document.querySelector(".facts-list");
-
-// Create DOM elements: Render facts in list
 facts_list.innerHTML = "";
 
-// facts_list.insertAdjacentHTML("afterbegin", "<li>Jonas</li><li>Mike</li>");
+async function loadFacts(api_key, supa_url) {
+  //(facts_list, category_list) {
+  const res = await fetch(supa_url, {
+    headers: {
+      apikey: api_key,
+      authorization: "Bearer " + api_key,
+    },
+  });
+  const data = await res.json(); // Note: the act of not including the await here, will print out a pending res!
+  return data;
+}
 
-const htmlArr = initialFacts.map(
-  (fact) => `<li class="fact">${fact.text}</li>`
+function createFactList(dataArray, fl, category_list) {
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+        <p>
+        ${fact.text}
+        <a
+          class="source"
+          href="${fact.source}"
+          target="_blank"
+          >(Source)</a
+        >
+        </p>
+        <span class="tag" style="background-color: ${
+          category_list.find((value) => {
+            // find returns the first, filter returns all and then have to index [0]
+            return value.name == fact.category;
+          }).color
+        }"
+        >${fact.category}</span
+        >
+        <div class="vote-buttons">
+          <button>👍 ${fact.votesInteresting}</button>
+          <button>🤯 ${fact.votesMindblowing}</button>
+          <button>⛔️ ${fact.votesFalse}</button>
+        </div>  
+    </li>`
+  );
+  const html = htmlArr.join("");
+  fl.insertAdjacentHTML("afterbegin", html);
+}
+
+loadFacts(API_KEY, SUPA_URL).then(
+  (response) => {
+    createFactList(response, facts_list, CATEGORIES);
+  },
+  (err) => {
+    console.log("Failed to connect to SupaBase");
+  }
 );
-facts_list.insertAdjacentHTML("afterbegin", htmlArr.join(""));
 
-// Toggle form visibility
+// *************** Toggle form visibility
+
+// Selecting DOM elements
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
